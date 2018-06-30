503d878a510581a31c3bb00df6e72e8c65342464
guansunyata <guansunyata@gmail.com>
2018-06-30 20:02:28 +0800

    Add simple example

---
+++ example/example.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="Example">
+    <table>
+      <thead>
+        <tr>
+          <td>Quantity</td>
+          <td>Limitation</td>
+        </tr>
+      </thead>
+      <tr 
+        v-for="item in list"
+        :key="item.id"
+      >
+        <td>
+          <input
+            type="number"
+            v-model="item.qty"
+            @change="onInputChange"
+          >
+          <label>
+            Quantity:
+          </label>
+          <!-- take error status by accessing property `validator_state[${id}]`. -->
+          <span
+            v-show="validator_state[item.id].qty.error"
+            class="error"
+          >
+            {{ validator_state[item.id].qty.errorMsg }}
+          </span>
+        </td>
+        <td>
+          {{ item.limit }}
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import validator from 'validator.mixin.js'
+
+// step1. define your rule object
+// apply your customize rules to any field you want.
+const rule1 = {
+  propertyName: 'qty', // property name of target field
+  validationFn: (item, list) => item.qty <= item.limit,
+  errorMsg: () => `the qty must be under the ${item.limit}`,
+}
+
+const rule2 = {
+  propertyName: 'limit',
+  validationFn: (item, list) => item.limit > 0,
+  errorMsg: 'the limitation must be larger than 0',
+}
+
+export default {
+  name: 'Example',
+  mixins: [
+    validator,
+  ],
+  data() {
+    return {
+      // target list
+      list: [
+        {
+          id: 0,
+          qty: 10,
+          limit: 5,
+        },
+        {
+          id: 1,
+          qty: 5,
+          limit: 10,
+        },
+      ],
+    }
+  },
+  created() {
+    // step 2. Apply your rule objects.
+    this.validator_rules = [
+      rule1,
+      rule2,
+    ]
+  },
+  computed: {
+    // step 3. Set your target list as computed property `validator_list` to be the portal of validator
+    validator_list() {
+      return this.list
+    },
+  },
+  methods: {
+    // step 4. Call `this.validateAll()` to validate target list whenever you want.
+    onInputChange() {
+      this.validateAll()
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+.error {
+  color: red;
+}
+</style>
